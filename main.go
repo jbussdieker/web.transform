264bd4f5cca876c711f9c5c01ec24f601f5047ad
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-01 23:40:09 -0700

    Simpler api in libxml. Html now and use hard coded filename.

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "libxml"
 import "io/ioutil"
 
 func serv(ctx *web.Context, message string) (resp string) {
-	out, err := ioutil.ReadFile(message)
+	out, err := ioutil.ReadFile("test.htm")
 	if err != nil {
 		ctx.Server.Logger.Print("ERROR: " + err.Error())
 		ctx.NotFound(err.Error())
@@ -13,11 +13,17 @@ func serv(ctx *web.Context, message string) (resp string) {
 	}
 	ctx.ContentType("html")
 
-	doc := libxml.XmlParseDoc(string(out))
-	doc.AddChild("a", "link")
-	buf := doc.Dump()
-	resp = buf.String()
-	buf.Free()
+	doc := libxml.HtmlParseDoc(string(out))
+	doc.AddChild("a", "a link")
+	doc.AddChild("b", "a bold")
+	doc.AddChild("i", "an italic")
+	doc.AddChild("quote", "a quote")
+	doc.AddChild("span", "a span")
+	doc.AddChild("code", "a code")
+	doc.AddChild("font", "a font")
+	doc.AddChild("p", "a paragraph")
+	doc.AddChild("div", "a div")
+	resp = doc.String()
 	doc.Free()
 
 	return
